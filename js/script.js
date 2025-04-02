ac81c13b9bfa18bd5c98c2314cbc82388c410f4e
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-04-02 17:30:30 +0000

    volume

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates wage.
+ */
+function calculate() {
+  // input
+  const radiusOfSphere = parseFloat(document.getElementById("radius").value)
+
+  // process
+  const volumeOfSphere = 4/3 * Math.PI * radiusOfSphere ** 3
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "volume is: " + volumeOfSphere.toFixed(3) + " mm³"
+}
